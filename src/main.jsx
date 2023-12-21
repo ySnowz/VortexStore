b6056f2ea3d08c5505a62cff246a3ba44181c089
ySnowz <loserss@outlook.pt>
2023-12-21 17:17:39 -0300

    fixing name component error

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/home'
+import Home from './pages/Home'
 import './styles/global.css'
 import Navbar from './components/Navbar'
 
